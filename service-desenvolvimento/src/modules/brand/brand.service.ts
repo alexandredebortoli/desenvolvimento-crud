d2c4ff971e2613d6eeef4b2af40240dd2dbc6980
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-06-29 02:01:47 -0300

    chore: added query comments to brand crud

--- service-desenvolvimento/src/modules/brand/brand.service.ts
+++ service-desenvolvimento/src/modules/brand/brand.service.ts
@@ -12,10 +12,12 @@ export class BrandService {
   ) {}
 
   async findAll(): Promise<BrandDTO[]> {
+    // MySQL query: SELECT * FROM desenvolvimentoDb.brand;
     return await this.brandRepository.find();
   }
 
   async findOne(id: number): Promise<BrandDTO> {
+    // MySQL query: SELECT * FROM desenvolvimentoDb.brand WHERE id = <id>;
     const brand = await this.brandRepository.findOne({ where: { id } });
     if (!brand) {
       throw new NotFoundException(`Brand with ID ${id} not found`);
@@ -24,16 +26,19 @@ export class BrandService {
   }
 
   async create(data: BrandInputDTO): Promise<BrandDTO> {
+    // MySQL query: INSERT INTO desenvolvimentoDb.brand (name) VALUES (<data.name>);
     const brand = this.brandRepository.create(data);
     return await this.brandRepository.save(brand);
   }
 
   async update(id: number, data: BrandInputDTO): Promise<BrandDTO> {
+    // MySQL query: UPDATE desenvolvimentoDb.brand SET name = <data.name> WHERE id = <id>;
     await this.brandRepository.update(id, data);
     return await this.findOne(id);
   }
 
   async delete(id: number): Promise<BrandDTO> {
+    // MySQL query: DELETE FROM desenvolvimentoDb.brand WHERE id = <id>;
     const brand = await this.findOne(id);
     await this.brandRepository.delete(id);
     return brand;
