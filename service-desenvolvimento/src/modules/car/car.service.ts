c52e521b1f4206d7d7a773c0600f9bd477093401
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-06-29 18:41:39 -0300

    chore: added query comments to car crud

--- service-desenvolvimento/src/modules/car/car.service.ts
+++ service-desenvolvimento/src/modules/car/car.service.ts
@@ -12,10 +12,12 @@ export class CarService {
   ) {}
 
   async findAll(): Promise<CarDTO[]> {
+    // MySQL query: SELECT * FROM desenvolvimentoDb.car;
     return await this.carRepository.find();
   }
 
   async findOne(id: number): Promise<CarDTO> {
+    // MySQL query: SELECT * FROM desenvolvimentoDb.car WHERE id = <id>;
     const car = await this.carRepository.findOne({ where: { id } });
     if (!car) {
       throw new NotFoundException(`Car with ID ${id} not found`);
@@ -24,16 +26,28 @@ export class CarService {
   }
 
   async create(data: CarInputDTO): Promise<CarDTO> {
+    // MySQL query: INSERT INTO desenvolvimentoDb.car
+    // (name, renavam, license, price, year, id_model)
+    // VALUES (<data.name>, <data.renavam>, <data.license>, <data.price>, <data.year>, <data.idModel>);
     const car = this.carRepository.create(data);
     return await this.carRepository.save(car);
   }
 
   async update(id: number, data: CarInputDTO): Promise<CarDTO> {
+    // MySQL query: UPDATE desenvolvimentoDb.car SET
+    //    name = <data.name>
+    //    renavam = <data.renavam>
+    //    license = <data.license>
+    //    price = <data.price>
+    //    year = <data.year>
+    //    id_model = <data.idModel>
+    // WHERE id = <id>;
     await this.carRepository.update(id, data);
     return await this.findOne(id);
   }
 
   async delete(id: number): Promise<CarDTO> {
+    // MySQL query: DELETE FROM desenvolvimentoDb.car WHERE id = <id>;
     const car = await this.findOne(id);
     await this.carRepository.delete(id);
     return car;
