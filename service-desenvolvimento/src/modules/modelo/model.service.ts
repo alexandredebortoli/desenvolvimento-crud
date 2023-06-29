b366e8cf44291a2665caca8b9a7c6adbc404ebf7
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-06-29 18:38:01 -0300

    chore: added query comments to model crud

--- service-desenvolvimento/src/modules/modelo/model.service.ts
+++ service-desenvolvimento/src/modules/modelo/model.service.ts
@@ -12,10 +12,12 @@ export class ModelService {
   ) {}
 
   async findAll(): Promise<ModelDTO[]> {
+    // MySQL query: SELECT * FROM desenvolvimentoDb.model;
     return await this.modelRepository.find();
   }
 
   async findOne(id: number): Promise<ModelDTO> {
+    // MySQL query: SELECT * FROM desenvolvimentoDb.model WHERE id = <id>;
     const model = await this.modelRepository.findOne({ where: { id } });
     if (!model) {
       throw new NotFoundException(`Model with ID ${id} not found`);
@@ -24,16 +26,19 @@ export class ModelService {
   }
 
   async create(data: ModelInputDTO): Promise<ModelDTO> {
+    // MySQL query: INSERT INTO desenvolvimentoDb.model (name, id_brand) VALUES (<data.name>, <data.idBrand>);
     const model = this.modelRepository.create(data);
     return await this.modelRepository.save(model);
   }
 
   async update(id: number, data: ModelInputDTO): Promise<ModelDTO> {
+    // MySQL query: UPDATE desenvolvimentoDb.model SET name = <data.name>, id_brand = <data.IdBrand> WHERE id = <id>;
     await this.modelRepository.update(id, data);
     return await this.findOne(id);
   }
 
   async delete(id: number): Promise<ModelDTO> {
+    // MySQL query: DELETE FROM desenvolvimentoDb.model WHERE id = <id>;
     const model = await this.findOne(id);
     await this.modelRepository.delete(id);
     return model;
